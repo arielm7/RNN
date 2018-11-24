930d9dff8363c99fc3c750f784b04d175e0b1499
arielm7 <arielmv07@gmail.com>
2018-11-23 18:21:42 -0600

    para 10  numeros

---
+++ audioClass.py
@@ -0,0 +1,220 @@
+import glob
+import os
+import librosa
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+#%matplotlib inline
+plt.style.use('ggplot')
+
+def windows(data, window_size):
+    start = 0
+    while start < len(data):
+        yield start, start + window_size
+        start += (window_size // 2)
+
+def extract_features(parent_dir,sub_dirs,file_ext="*.wav",bands = 20, frames = 41):
+    window_size = 512 * (frames - 1)
+    mfccs = []
+    labels = []
+    for l, sub_dir in enumerate(sub_dirs):
+        for fn in glob.glob(os.path.join(parent_dir, sub_dir, file_ext)):
+            sound_clip,s = librosa.load(fn)
+            label = fn.split('fold')[1].split('/')[0]
+            for (start,end) in windows(sound_clip,window_size):
+                if(len(sound_clip[start:end]) == window_size):
+                    print(fn)
+                    signal = sound_clip[start:end]
+                    mfcc = librosa.feature.mfcc(y=signal, sr=s, n_mfcc = bands).T.flatten()[:, np.newaxis].T
+                    mfccs.append(mfcc)
+                    labels.append(label)         
+    features = np.asarray(mfccs).reshape(len(mfccs),bands,frames)
+    return np.array(features), np.array(labels,dtype = np.int)
+
+def one_hot_encode(labels):
+    n_labels = len(labels)
+    n_unique_labels = len(np.unique(labels))
+    one_hot_encode = np.zeros((n_labels,10))
+    #print(one_hot_encode)
+    #one_hot_encode[np.arange(n_labels), labels] = 1
+    for i in range(n_labels):
+        one_hot_encode[i][labels[0]-1] = 1
+    
+    return one_hot_encode
+
+    # use this to process the audio files into numpy arrays
+def save_folds(data_dir):
+    for k in range(1,11):
+        fold_name = 'fold' + str(k)
+        print("\nSaving " + fold_name)
+        features, labels = extract_features(parent_dir, [fold_name])
+        labels = one_hot_encode(labels)
+        
+        #print("Features of", fold_name , " = ", features.shape)
+        #print("Labels of", fold_name , " = ", labels.shape)
+        
+        feature_file = os.path.join(data_dir, fold_name + '_x.npy')
+        labels_file = os.path.join(data_dir, fold_name + '_y.npy')
+        np.save(feature_file, features)
+        #print("Saved " + feature_file)
+        np.save(labels_file, labels)
+        #print("Saved " + labels_file)
+        #print(labels)
+
+def assure_path_exists(path):
+    mydir = os.path.join(os.getcwd(), path)
+    if not os.path.exists(mydir):
+        os.makedirs(mydir)
+        
+#uncomment this to recreate and save the feature vectors
+parent_dir = "/home/ariel/TEC/patrones/proyecto2/audioRNN/" # Where you have saved the UrbanSound8K data set"       
+save_dir = "/home/ariel/TEC/patrones/proyecto2/audioRNN/data"
+assure_path_exists(save_dir)
+#save_folds(save_dir)
+
+# this is used to load the folds incrementally
+def load_folds(folds):
+    subsequent_fold = False
+    for k in range(len(folds)):
+        fold_name = 'fold' + str(folds[k])
+        feature_file = os.path.join(data_dir, fold_name + '_x.npy')
+        labels_file = os.path.join(data_dir, fold_name + '_y.npy')
+        loaded_features = np.load(feature_file)
+        loaded_labels = np.load(labels_file)
+        print (fold_name, "features: ", loaded_features.shape)
+
+        if subsequent_fold:
+            features = np.concatenate((features, loaded_features))
+            labels = np.concatenate((labels, loaded_labels))
+        else:
+            features = loaded_features
+            labels = loaded_labels
+            subsequent_fold = True
+        
+    return features, labels
+data_dir = "/home/ariel/TEC/patrones/proyecto2/audioRNN/data"
+
+def extract_feature_array(filename, bands = 20, frames = 41):
+    window_size = 512 * (frames - 1)
+    mfccs = []
+    sound_clip,s = librosa.load(filename)
+    for (start,end) in windows(sound_clip,window_size):
+          
+        if(len(sound_clip[start:end]) == window_size):
+           
+            signal = sound_clip[start:end]
+            mfcc = librosa.feature.mfcc(y=signal, sr=s, n_mfcc = bands).T.flatten()[:, np.newaxis].T
+            mfccs.append(mfcc)
+           
+            
+    features = np.asarray(mfccs).reshape(len(mfccs),bands,frames)
+    return np.array(features)
+
+sample_filename = "samples/us8k/music.wav"
+features = extract_feature_array(sample_filename)
+data_points, _ = librosa.load(sample_filename)
+print ("IN: Initial Data Points =", len(data_points))
+print ("OUT: Total features =", np.shape(features))    
+
+tf.set_random_seed(0)
+np.random.seed(0)
+
+def evaluate(model):
+    y_prob = model.predict_proba(test_x, verbose=0)
+    y_pred = y_prob.argmax(axis=-1)
+    y_true = np.argmax(test_y, 1)
+
+    #roc = roc_auc_score(test_y, y_prob)
+    #print ("ROC:",  round(roc,3))
+
+    # evaluate the model
+    score, accuracy = model.evaluate(test_x, test_y, batch_size=32)
+    print("\nAccuracy = {:.2f}".format(accuracy))
+
+    # the F-score gives a similiar value to the accuracy score, but useful for cross-checking
+    p,r,f,s = precision_recall_fscore_support(y_true, y_pred, average='micro')
+    print ("F-Score:", round(f,2))
+    
+    return accuracy
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Dropout
+from sklearn.metrics import precision_recall_fscore_support, roc_auc_score
+from keras.utils import np_utils
+from keras.callbacks import EarlyStopping
+
+data_dim = 41
+timesteps = 20
+num_classes = 10
+
+# expected input data shape: (batch_size, timesteps, data_dim)
+model = Sequential()
+
+# returns a sequence of vectors of dimension 512
+model.add(LSTM(150, return_sequences=True, input_shape=(timesteps, data_dim)))  
+
+#model.add(Dropout(0.2))
+
+# return a single vector of dimension 128
+model.add(LSTM(200))  
+
+#model.add(Dropout(0.2))
+
+# apply softmax to output
+model.add(Dense(num_classes, activation='softmax'))
+
+
+# compile the model for multi-class classification
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# load fold1 for testing
+train_x, train_y = load_folds([1,2,3,4,5,6,7,8,9,10])
+print("wwwwwww")
+#print(train_x.shape)
+#print(train_x)
+# load fold2 for validation
+valid_x, valid_y = load_folds([9])
+    
+# load fold3 for testing
+test_x, test_y = load_folds([7])
+
+# a stopping function to stop training before we excessively overfit to the training set
+earlystop = EarlyStopping(monitor='val_loss', patience=0, verbose=1, mode='auto')
+
+model.fit(train_x, train_y, batch_size=128, nb_epoch=80, callbacks=[earlystop], validation_data=(valid_x, valid_y))    
+
+print("Evaluating model...")
+acc = evaluate(model)
+
+
+sound_file_paths = ["uno_22_1.wav","dos_11_2.wav","tres_26_2.wav","cuatro_18_2.wav","cinco_30_3.wav", 
+"seis_27_3.wav","siete_7_3.wav","ocho_10_3.wav","nueve_25_3.wav","diez_24_1.wav"]
+sound_names = ["uno","dos","tres","cuatro","cinco","seis",
+               "siete","ocho","nueve","diez"]
+parent_dir = 'samples/digits/'
+
+
+# create predictions for each of the sound classes
+for s in range(len(sound_file_paths)):
+
+    print ("\n----- ", sound_names[s], "-----")
+    # load audio file and extract features
+    predict_file = parent_dir + sound_file_paths[s]
+    predict_x = extract_feature_array(predict_file)
+    
+    # generate prediction, passing in just a single row of features
+    predictions = model.predict(predict_x)
+    print(predictions)
+    if len(predictions) == 0: 
+        print ("No prediction")
+        continue
+    
+    #for i in range(len(predictions[0])):
+    #    print sound_names[i], "=", round(predictions[0,i] * 100, 1)
+    
+    # get the indices of the top 2 predictions, invert into descending order
+    ind = np.argpartition(predictions[0], -2)[-2:]
+    ind[np.argsort(predictions[0][ind])]
+    ind = ind[::-1]
+    
+    print ("Top guess: ", sound_names[ind[0]], " (",round(predictions[0,ind[0]],3),")")
+    print ("2nd guess: ", sound_names[ind[1]], " (",round(predictions[0,ind[1]],3),")")
---
+++ audioSignal.py
@@ -0,0 +1,187 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm 
+from scipy.io import wavfile
+from scipy.fftpack import dct
+from python_speech_features import mfcc, logfbank
+
+'''
+@brief class to extract the characteristics of the audio file to analize
+'''
+class AudioSignal:
+    def __init__(self,pFileName):
+        self.fileName = pFileName
+        self.frequencySampling = None 
+        self.audioSignal = None 
+        self.featuresMFCC = None
+        self.filterbankFeatures = None        
+        self.plotFlag = False
+    
+    '''
+    @brief set the audio file name and directory (must be a .wav format)
+    @param pFileName the directory of the file
+    '''
+    def setFileName(self, pFileName):
+        self.fileName = pFileName
+
+    '''
+    @brief set the audio frequency sampling
+    @param pFrequencySampling the value
+    '''
+    def setFrequencySampling(self, pFrequencySampling):
+        self.frequencySampling = pFrequencySampling
+
+    '''
+    @brief set the audio audio signal characterization
+    @param self the directory of the file
+    '''
+    def setAudioSignal(self, pAudioSignal):
+        self.audioSignal = pAudioSignal
+
+    '''
+    @brief set the audio mfcc features
+    @param pMFCC
+    '''
+    def setMFCCFeature(self, pMFCC):
+        self.featuresMFCC = pMFCC
+
+    '''
+    @brief set the audio filter bank Features
+    @param pFilterbankFeatures
+    '''
+    def setFilterBankFeature(self, pFilterbankFeatures):
+        self.filterbankFeatures = pFilterbankFeatures
+    
+    '''
+    @brief methods that get the frequency and the audio signal of an audio file
+    @returns frequency_sampling, audioSignal
+    '''
+    def getAudioSignalFrequency(self):
+        try:
+            self.frequencySampling, self.audioSignal = wavfile.read(self.fileName)
+            pre_emphasis = 0.97
+            self.audioSignal = np.append(self.audioSignal[0], self.audioSignal[1:] - pre_emphasis * self.audioSignal[:-1])
+        except:
+            print("Unable to get frequency_sampling, audioSignal from " + self.fileName)
+            return 
+
+    '''
+    @brief normalize the energy
+    '''
+    def normalizeEnergy(self):
+        from pydub import AudioSegment
+
+        def match_target_amplitude(sound, target_dBFS):
+            change_in_dBFS = target_dBFS - sound.dBFS
+            return sound.apply_gain(change_in_dBFS)
+
+        sound = AudioSegment.from_file(self.fileName, "wav")
+        normalized_sound = match_target_amplitude(sound, -20.0)
+        normalized_sound.export("./datos_normalizados/" + self.fileName.split("/")[-1], format="wav")
+        self.fileName = "./datos_normalizados/" + self.fileName.split("/")[-1]
+        self.getAudioSignalFrequency()
+        
+
+        #if(self.plotFlag):
+        #   self.plotSignal('Frequency (kHz)', 'Signal power (dB)', 'Freq vs Power', 1000.0, lengthSignal, len_fts, 1, self.signalPower)
+
+    '''
+    '''
+    def signalFeatures(self, frame_size, frame_stride):        
+        # params
+        frame_length, frame_step = frame_size * self.frequencySampling, frame_stride * self.frequencySampling  # Convert from seconds to samples
+        signal_length = len(self.audioSignal)
+        frame_length = int(round(frame_length))
+        frame_step = int(round(frame_step))
+        num_frames = int(np.ceil(float(np.abs(signal_length - frame_length)) / frame_step))  # Make sure that we have at least 1 frame
+
+        pad_signal_length = num_frames * frame_step + frame_length
+        z = np.zeros((pad_signal_length - signal_length))
+        pad_signal = np.append(self.audioSignal, z) # Pad Signal to make sure that all frames have equal number of samples without truncating any samples from the original signal
+
+        indices = np.tile(np.arange(0, frame_length), (num_frames, 1)) +\
+            np.tile(np.arange(0, num_frames * frame_step, frame_step), (frame_length, 1)).T
+        frames = pad_signal[indices.astype(np.int32, copy=False)]
+
+        # hamming window
+        frames *= np.hamming(frame_length)
+
+        NFFT = 512
+        mag_frames = np.absolute(np.fft.rfft(frames, NFFT))  # Magnitude of the FFT
+        pow_frames = ((1.0 / NFFT) * ((mag_frames) ** 2))  # Power Spectrum
+
+        nfilt = 40
+        low_freq_mel = 0
+        high_freq_mel = (2595 * np.log10(1 + (self.frequencySampling / 2) / 700))  # Convert Hz to Mel
+        mel_points = np.linspace(low_freq_mel, high_freq_mel, nfilt + 2)  # Equally spaced in Mel scale
+        hz_points = (700 * (10**(mel_points / 2595) - 1))  # Convert Mel to Hz
+        bin = np.floor((NFFT + 1) * hz_points / self.frequencySampling)
+
+        fbank = np.zeros((nfilt, int(np.floor(NFFT / 2 + 1))))
+        for m in range(1, nfilt + 1):
+            f_m_minus = int(bin[m - 1])   # left
+            f_m = int(bin[m])             # center
+            f_m_plus = int(bin[m + 1])    # right
+
+            for k in range(f_m_minus, f_m):
+                fbank[m - 1, k] = (k - bin[m - 1]) / (bin[m] - bin[m - 1])
+            for k in range(f_m, f_m_plus):
+                fbank[m - 1, k] = (bin[m + 1] - k) / (bin[m + 1] - bin[m])
+        self.filterbankFeatures = np.dot(pow_frames, fbank.T)
+        self.filterbankFeatures = np.where(self.filterbankFeatures == 0, np.finfo(float).eps, self.filterbankFeatures)  # Numerical Stability
+        self.filterbankFeatures = 20 * np.log10(self.filterbankFeatures)  # dB
+        
+        num_ceps = 20
+        low_freq_mel = 0
+        self.featuresMFCC = dct(self.filterbankFeatures, type=2, axis=1, norm='ortho')[:, 1 : (num_ceps + 1)] # Keep 2-13
+        
+        cep_lifter = 22
+        low_freq_mel = 0
+        (nframes, ncoeff) = self.featuresMFCC.shape
+        n = np.arange(ncoeff)
+        lift = 1 + (cep_lifter / 2) * np.sin(np.pi * n / cep_lifter)
+        low_freq_mel = 0
+        self.featuresMFCC *= lift  #*
+
+
+    '''
+    @brief method to plot the audio signal
+    '''
+    def plotSignal(self,pXLabel, pYLabel, pTitle, pAmount, pSize, pSize2, pType, pSignal):
+        if(pType ==0): 
+            time_axis = pAmount * np.arange(0, len(pSignal), 1) / float(self.frequencySampling)
+        else: 
+            time_axis = np.arange(0, pSize2, 1)*(self.frequencySampling / pSize) / pAmount
+        plt.plot(time_axis, pSignal, color='blue')
+        plt.xlabel(pXLabel)
+        plt.ylabel(pYLabel)
+        plt.title(pTitle)
+
+    def normalized_fb(self):
+        self.filterbankFeatures -= (np.mean(self.filterbankFeatures, axis=0) + 1e-8)
+
+
+    def normalized_mfcc(self):
+        self.featuresMFCC -= (np.mean(self.featuresMFCC, axis=0) + 1e-8)
+
+    '''
+    @brief plot the mfcc features
+    @param pFeaturesMFCC the mfcc features
+    '''
+    def pltoMFCCFeatures(self):
+        pFeaturesMFCC = self.featuresMFCC.T
+        plt.matshow(pFeaturesMFCC)
+        #plt.imshow(np.flipud(pFeaturesMFCC.T), cmap=cm.jet, aspect=0.2, extent=[0,T,0,4])
+        plt.title('MFCC Features')
+        plt.savefig("./images/MFCC/"+self.fileName.split("/")[-1].split(".")[0].split("_")[0]+"_"+self.fileName.split("/")[-1].split(".")[0])
+
+    '''
+    @brief plot the mfcc features
+    @param pFeaturesMFCC the mfcc features
+    '''
+    def pltoFilterBankFeatures(self):
+        filterbankFeatures = self.filterbankFeatures.T
+        plt.matshow(filterbankFeatures)
+        #plt.imshow(np.flipud(filterbankFeatures.T), cmap=cm.jet, aspect=0.2, extent=[0,T,0,4])
+        plt.title('Filter bank')
+        plt.savefig("./images/FB/"+self.fileName.split("/")[-1].split(".")[0].split("_")[0]+"_"+self.fileName.split("/")[-1].split(".")[0])
